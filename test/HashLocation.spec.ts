c590ddebe6c709439aaa6234777d71d2bf519559
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-05 21:26:20 +0800

    add more tests for location provider

--- test/HashLocation.spec.ts
+++ test/HashLocation.spec.ts
@@ -1,11 +1,21 @@
 /// <reference path="../typings/globals/mocha/index.d.ts" />
 /// <reference path="../typings/globals/chai/index.d.ts" />
 
-import { HashLocation } from "../src/HashLocation";
+import { HashLocation as l } from "../src/HashLocation";
 import { LPSpec } from "./LocationProvider.helper";
+import * as _ from "lodash";
 
 const expect = chai.expect;
 
 describe("class HashLocation", () => {
-    LPSpec(HashLocation);
+    LPSpec(l);
+    it("modifies only hash part", () => {
+        const w = window.location;
+        let old  = _.merge<typeof w>({}, w);
+        l.go("qq");
+        expect(w.origin).to.equal(old.origin);
+        expect(w.pathname).to.equal(old.pathname);
+        expect(w.search).to.equal(old.search);
+        expect(w.hash).to.equal("#qq");
+    });
 });
--- test/HistoryLocation.spec.ts
+++ test/HistoryLocation.spec.ts
@@ -1,11 +1,21 @@
 /// <reference path="../typings/globals/mocha/index.d.ts" />
 /// <reference path="../typings/globals/chai/index.d.ts" />
 
-import { HistoryLocation } from "../src/HistoryLocation";
+import { HistoryLocation as l } from "../src/HistoryLocation";
 import { LPSpec } from "./LocationProvider.helper";
+import * as _ from "lodash";
 
 const expect = chai.expect;
 
 describe("class HistoryLocation", () => {
-    LPSpec(HistoryLocation);
+    LPSpec(l);
+    it("clears search and hash parts, and changes pathname part", () => {
+        const w = window.location;
+        let old  = _.merge<typeof w>({}, w);
+        l.go("/qq");
+        expect(w.origin).to.equal(old.origin);
+        expect(w.search).to.be.empty;
+        expect(w.hash).to.be.empty;
+        expect(w.pathname).to.equal("/qq");
+    });
 });
