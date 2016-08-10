faa17e0509dcb823dec478cca66b889c86dd928b
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-10 16:53:57 +0800

    Support url parameters
    It's a bad practice to squeeze so much codes in a commit. At this
    time, I'm too afraid to split it.

--- src/Route.tsx
+++ src/Route.tsx
@@ -10,34 +10,117 @@ export interface RouteState {
     show?: boolean;
 }
 
+export type ArgType = number | string | boolean;
+
+interface position {
+    pos: number;
+    c: (str: string) => ArgType;
+}
+
 export default class Route extends React.Component<RouteProps, RouteState> {
+    private regexp: RegExp;
+    private _args: ArgType[];
+    private _argPos: position[];
     constructor(props?: RouteProps, context?: any) {
-	super(props, context);
-	this.state = { show: this.shellWeShow(this.props.loc.current) };
-	this.props.loc.senpai(this.pathChanged);
+        super(props, context);
+        this.state = { show: false };
+        this.props.loc.senpai(this.pathChanged);
+
+        this._args = [];
+        this._argPos = [];
+        this.buildRegexp();
+    }
+
+    componentWillMount() {
+        this.parse();
+    }
+
+    toString(str: string): ArgType {
+        return str;
+    }
+
+    toBoolean(str: string): ArgType {
+        switch (str.toLowerCase()) {
+            case "false":
+            case "f":
+            case "n":
+            case "0":
+                return false;
+        }
+
+        return true;
+    }
+
+    toNumber(str: string): ArgType {
+        return (new Number(str)).valueOf();
+    }
+
+    private buildRegexp() {
+        const numberRegexp = "(-?[0-9]+(\.[0-9]*[1-9])?)";
+        const stringRegexp = "([^/]*)";
+        const booleanRegexp = stringRegexp;
+
+        let curPos = 1; // first argument is matched_array[1]
+        let arr = this.props.path.split("/").map((v: string, i: number) => {
+            switch (v) {
+                case ":n": // number
+                    this._argPos.push({ pos: curPos, c: this.toNumber });
+                    curPos += 2; // needs two entries in matched array
+                    return numberRegexp;
+                case ":s": // string
+                    this._argPos.push({ pos: curPos, c: this.toString });
+                    curPos++;
+                    return stringRegexp;
+                case ":b":
+                    this._argPos.push({ pos: curPos, c: this.toBoolean });
+                    curPos++;
+                    return booleanRegexp;
+            }
+            return v;
+        });
+
+        this.regexp = new RegExp(arr.join("/"));
+    }
+
+    get args(): ArgType[] {
+        return this._args;
+    }
+
+    private parse() {
+        this._args = [];
+        let matched = this.regexp.exec(this.props.loc.current);
+        if (!matched) {
+            this.show(false);
+            return;
+        }
+
+        this.show(true);
+
+        let cur = 0;
+        for (let pos = 0; pos < matched.length && cur < this._argPos.length; pos++) {
+	    const argpos = this._argPos[cur];
+            if (pos === argpos.pos) {
+                this.args.push(argpos.c(matched[pos]));
+                cur++;
+            }
+        }
     }
 
     render() {
-	if (this.state.show) {
-	    return React.Children.only(this.props.children) as React.ReactElement<any>;
-	}
+        if (this.state.show) {
+            return React.Children.only(this.props.children) as React.ReactElement<any>;
+        }
 
-	return null;
+        return null;
     }
 
-    show(visible: boolean) {
-	if (this.state.show !== visible) {
-	    this.setState({ show: visible });
-	}
+    private show(visible: boolean) {
+        if (this.state.show !== visible) {
+            this.setState({ show: visible });
+        }
     }
 
-    pathChanged: (path: string) => void = (path: string) => {
-	this.show(this.shellWeShow(path));
+    private pathChanged: (path: string) => void = (path: string) => {
+        this.parse();
     };
-
-    shellWeShow(path: string): boolean {
-	// matching with path
-	// support only precise match currently
-	return path === this.props.path;
-    }
 }
--- test/Route.spec.tsx
+++ test/Route.spec.tsx
@@ -29,4 +29,77 @@ describe("<Route />", () => {
 
 	expect(wrapper.children()).to.have.length(0);
     });
+
+    describe("regexp", () => {
+	describe("strings(:s)", () => {
+	    it("accepts strings as-is", () => {
+		l.go("/string/test/case");
+		let wrapper = shallow(
+		    <Route path="/string/:s" loc={l}>
+			<div />
+		    </Route>
+		);
+
+		expect(wrapper.is("div")).to.be.true;
+		const args = (wrapper.instance() as Route).args;
+		expect(args).to.deep.equal(["test"]);
+	    });
+	});
+	describe("numbers(:n)", () => {
+	    function test(path: string, num: number) {
+		l.go("/number/" + path);
+		let wrapper = shallow(
+		    <Route path="/number/:n" loc={l}>
+			<div />
+		    </Route>
+		);
+
+		expect(wrapper.is("div")).to.be.true;
+		const args = (wrapper.instance() as Route).args;
+		expect(args).to.deep.equal([num]);
+	    }
+
+	    it("accepts zero", test.bind({}, "0", 0));
+	    it("accepts positive integer like 1", test.bind({}, "1", 1));
+	    it("accepts negative integer like -1", test.bind({}, "-1", -1));
+	    it("accepts positive float like 0.5", test.bind({}, "0.5", 0.5));
+	    it("accepts negative float like -0.5", test.bind({}, "-0.5", -0.5));
+	    it("fails on format error", () => {
+		l.go("/number/string");
+		let wrapper = shallow(
+		    <Route path="/number/:n" loc={l}>
+			<div />
+		    </Route>
+		);
+
+		expect(wrapper.children()).to.have.length(0);
+	    });
+	});
+	describe("booleans(:b)", () => {
+	    function test(path: string, bool: boolean) {
+		l.go("/bool/" + path);
+		let wrapper = shallow(
+		    <Route path="/bool/:b" loc={l}>
+			<div />
+		    </Route>
+		);
+
+		expect(wrapper.is("div")).to.be.true;
+		const args = (wrapper.instance() as Route).args;
+		expect(args).to.deep.equal([bool]);
+	    }
+	    
+	    it("accepts false as false", test.bind({}, "false", false));
+	    it("accepts f as false", test.bind({}, "f", false));
+	    it("accepts n as false", test.bind({}, "n", false));
+	    it("accepts 0 as false", test.bind({}, "0", false));
+	    it("accepts others as true", test.bind({}, "I can swallow glasses without hurting myself", true));
+	    it("is case-insensitive", () => {
+		test("False", false);
+		test("FALSE", false);
+		test("F", false);
+		test("N", false);
+	    });
+	});
+    });
 });
